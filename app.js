e7c86150548107e7974c59ecf4d54c59e50ca7ba
JamieRez <jamierezendes@gmail.com>
2018-12-05 17:49:37 -0800

    voice chat fix

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.use(bodyParser());
 app.use(cookieParser());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin,
-  X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin");
   next();
 });
 app.use('/peerjs', peerServer);
